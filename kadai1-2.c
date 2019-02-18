beede324e092d0db4a4fc5c9534ec808ac2f804d
Junta Matsukidaira 松木平 淳太 <junta@mail.ryukoku.ac.jp>
2019-02-18 17:05:46 +0900

    Create kadai1-2.c

---
+++ kadai1-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{ 
+  int i;
+  int data[] = { 31, 41, 59, 26, 53, 58, 97, 93, 23, 84 };
+  int max_data = data[0];
+
+  for (i = 1; i < sizeof(data) / sizeof(data[0]); i++) {
+      /* ここを埋める */
+  }
+
+  printf("最大値は %d です。\n", max_data);
+}

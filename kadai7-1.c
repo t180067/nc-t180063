72aca821c9ae9d627decd19d1925b2ef4821317e
Junta Matsukidaira 松木平 淳太 <junta@mail.ryukoku.ac.jp>
2019-02-18 17:25:01 +0900

    Create kadai7-1.c

---
+++ kadai7-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+double f(double x)
+{
+    return 『関数の中身』;
+}
+
+double df(double x)
+{
+    return 『関数の中身』;
+}
+
+int main () 
+{
+  /* ここにニュートン法のアルゴリズムを書く */
+}

14cf44c7990ec6d182bd2be8f7f4441c3f39bf55
t180063 <49538821+t180063@users.noreply.github.com>
2019-04-12 11:26:39 +0900

    Update kadai1-2.c

--- kadai1-2.c
+++ kadai1-2.c
@@ -5,7 +5,7 @@ int main ()
   int i;
   int data[] = { 31, 41, 59, 26, 53, 58, 97, 93, 23, 84 };
   int max_data = data[0];
- //配列の要素数＝配列の要素全体の大きさ/配列の要素ひとつ分の大きさ
+
   for (i = 1; i < sizeof(data) / sizeof(data[0]); i++) {
       if(max_data<data[i])
         max_data=data[i];

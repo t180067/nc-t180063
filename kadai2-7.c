f4d1a8d1600ae5199d509bbaa66232d50f46eaa7
t180063 <49538821+t180063@users.noreply.github.com>
2019-04-19 16:55:40 +0900

    Update kadai2-7.c

--- kadai2-7.c
+++ kadai2-7.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
+	
+ long long fac (long long n)
+{
+  return (n == 0) ? 1 : n*fac(n-1);
+}
 
+ int main ()
+{
+  long long i;
+  for (i = 1; i <= 16; i++) {
+    printf("%2lld! = %lld\n", i, fac(i));
+  }
+}

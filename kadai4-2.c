8790b89964aa2340f1d20f89be2898455036cb17
t180063 <49538821+t180063@users.noreply.github.com>
2019-05-10 11:15:04 +0900

    Update kadai4-2.c

--- kadai4-2.c
+++ kadai4-2.c
@@ -11,16 +11,3 @@ int main(){
 
     }
 }
-
-#include<stdio.h>
-
-int main(){
-    int i,a0=1,N;
-    printf("正の整数を入力");
-    scanf("%d",&N);
-
-    for(i=0; i<=N; i++){
-        printf("a%d=%d\n",i,a0);
-         a0=a0*2;
-
-    }

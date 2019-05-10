48d53c0d5b53ccac48b5dbae50d49ac7acfb9f51
t180063 <49538821+t180063@users.noreply.github.com>
2019-05-10 11:29:27 +0900

    Update kadai4-4.c

--- kadai4-4.c
+++ kadai4-4.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    double a0=1.0,s=0;
+    int i,N;
+    printf("正の整数を入力");
+    scanf("%d",&N);
+
+    for(i=0; i<=N; i++){
+        s+=a0;
+        printf("S%d=%f\n",i,s);
+         a0*=0.5;
+
+    }
+}
 
